af5ad25c6a6b65b70c69d393c9c64f25491ba20b
sachinjangam29 <Sachinjagannathjangam29@gmail.com>
2024-12-17 01:57:25 +0530

    first commit

---
+++ backend/src/test/java/org/expense/tracker/budget_service/BudgetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.expense.tracker.budget_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
